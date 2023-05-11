166bb804b0d747199f0ab625514aabe5b89c5ca9
ChristinRobinson <christinrobinson463@gmail.com>
2023-05-11 15:42:39 +0530

    Revert "Imports & Exports in ES6"
    This reverts commit 4d60113cd5d189667d4c9fe75e374f99d5a2525e.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-// Imports & Exports
-import Imports from './01 Imports & Exports/Imports';
-
 // Class Component Example
 class ClassComponentExample extends React.Component {
 	render() {
 		return (
 			// You can use <React.Fragment> as the parent instead of <div> if you want to avoid adding an extra wrapper element in the HTML structure.
 			<React.Fragment>
-				<h2>Class Component Example!</h2>
+				<h1>Class Component Example!</h1>
 			</React.Fragment>
 		);
 	}
@@ -20,7 +17,7 @@ const FunctionalComponentExample = () => {
 	return (
 		// You can use <React.Fragment> as the parent instead of <div> if you want to avoid adding an extra wrapper element in the HTML structure.
 		<React.Fragment>
-			<h2>Functional Component Example!</h2>
+			<h1>Functional Component Example!</h1>
 		</React.Fragment>
 	);
 };
@@ -30,19 +27,8 @@ const App = () => {
 		// JSX Expressions must have one parent element
 		<div className='App'>
 			<h1>Hello, World!</h1>
-			<hr />
-
-			<h1>
-				<u>Types of Components</u>
-			</h1>
 			<ClassComponentExample />
 			<FunctionalComponentExample />
-			<hr />
-
-			<h1>
-				<u>Imports / Exports</u>
-			</h1>
-			<Imports />
 		</div>
 	);
 };
