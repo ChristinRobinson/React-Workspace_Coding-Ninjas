37dffb441e535641a4dbcfc95265d039349af249
ChristinRobinson <christinrobinson463@gmail.com>
2023-05-11 12:36:24 +0530

    Types of Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+// Class Component Example
+class ClassComponentExample extends React.Component {
+	render() {
+		return (
+			// You can use <React.Fragment> as the parent instead of <div> if you want to avoid adding an extra wrapper element in the HTML structure.
+			<React.Fragment>
+				<h1>Class Component Example!</h1>
+			</React.Fragment>
+		);
+	}
+}
+
+// Functional Component Example
+const FunctionalComponentExample = () => {
+	return (
+		// You can use <React.Fragment> as the parent instead of <div> if you want to avoid adding an extra wrapper element in the HTML structure.
+		<React.Fragment>
+			<h1>Functional Component Example!</h1>
+		</React.Fragment>
+	);
+};
+
 const App = () => {
 	return (
+		// JSX Expressions must have one parent element
 		<div className='App'>
 			<h1>Hello, World!</h1>
+			<ClassComponentExample />
+			<FunctionalComponentExample />
 		</div>
 	);
 };
