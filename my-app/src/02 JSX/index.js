9574470ec7709717f63e53b5bb4d363fbe6c9a7f
ChristinRobinson <christinrobinson463@gmail.com>
2023-05-12 10:06:30 +0530

    Conditional rendering with examples

--- my-app/src/02 JSX/index.js
+++ my-app/src/02 JSX/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 
 const JSXExamples = () => {
 	let name = 'Christin Robinson';
-	let loading = false;
-
-	if (loading) {
-		return <h2>Loading</h2>;
-	}
+	let isLoggedIn = false;
 
 	return (
 		<div className='jsx-example'>
-			<h2>Hello, {name}</h2>
+			{!isLoggedIn && <p>Hello, World!</p> }
+			{isLoggedIn && <h2>Hello, {name}</h2>}
 		</div>
 	);
 };
